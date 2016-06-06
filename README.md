96ffe1b920c09d9cb9cca67555e2a235aa93e56c
Jason Eppink <jason.eppink@gmail.com>
2016-06-06 20:04:17 +0000

    consolidate dbus calls in Python script

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 Displays subtitles on an external graphic LCD display synchronized to a video.
 
-v.1 (2016-06-01): Synchornization works, but omxplayer dbus times out after 4 hours 38 minutes. No interface with the graphic LCD display yet.
+v0.2 (2016-06-06): omxplayer dbus time out problem identified as problem with omxplayer; this is fixed with recent updates. v.2 consolidates dbus calls in the Python script (eliminating the need for additional bash scripts)
+v0.1 (2016-06-01): Synchornization works, but omxplayer dbus times out after 4 hours 38 minutes. No interface with the graphic LCD display yet.
--- extsub.py
+++ extsub.py
@@ -6,10 +6,9 @@ import srt
 import sys
 from pprint import pprint
 import time
-from datetime import datetime
-import logging
-
-#logging.basicConfig(filename='log.log',level=logging.DEBUG)
+#from datetime import datetime
+import getpass
+import dbus
 
 if(len(sys.argv) > 1):
 	video = sys.argv[1]
@@ -37,15 +36,33 @@ def tc_to_ms(s):
 #omxplayer's timecode resolution is much higher than SRT files so this chops off the last 3 digits
 def chop_digits(s):
         if long(s) > 999:
-                return s[:-4]
+                return s[:-3]
         else:
                 return "0"
 
-			#disable subtitles (if name of video file and SRT file are the same, omxplayer automatically turns them on)
-			#dbusIfaceKey.Action(dbus.Int32("30"))
 #start omxplayer
 cmd = "omxplayer --no-osd --loop %s" %(video)
 Popen([cmd], shell=True)
+
+#start dbus
+done, retry = 0, 0
+while done==0:
+	try:
+		with open('/tmp/omxplayerdbus.' + getpass.getuser(), 'r+') as f:
+			omxplayerdbus = f.read().strip()
+		bus = dbus.bus.BusConnection(omxplayerdbus)
+		object = bus.get_object('org.mpris.MediaPlayer2.omxplayer','/org/mpris/MediaPlayer2', introspect=False)
+		dbusIfaceProp = dbus.Interface(object,'org.freedesktop.DBus.Properties')
+		dbusIfaceKey = dbus.Interface(object,'org.mpris.MediaPlayer2.Player')
+		#disable in-player subtitles on video
+		dbusIfaceKey.Action(dbus.Int32("30"))
+		done=1
+	except:
+		retry+=1
+		if retry >= 5000:
+			print "ERROR"
+			raise SystemExit
+
 
 #import SRT subtitle file as object
 with open(subs, 'r') as myfile:
@@ -60,7 +77,7 @@ position = "0"
 duration = "0"
 while duration == "0":
 	try:
-		duration = chop_digits(subprocess.check_output(['./get_duration.sh']))
+		duration = chop_digits(str(dbusIfaceProp.Duration()))
 	except:
 		pass
 
@@ -68,7 +85,7 @@ while long(duration) > long(position):
 	#sys.stdout.write("position: " + str(position) + "\n")
 	start = tc_to_ms(str(subtitles[i].start))
 	end = tc_to_ms(str(subtitles[i].end))
-	position = chop_digits(subprocess.check_output(['./get_position.sh']))
+	position = chop_digits(str(dbusIfaceProp.Position()))
 
 	#sys.stdout.write("d: " + duration + " s: " + str(start) + " p: " + str(position) + " e: " + str(end) + " i: " + str(i) + "\n")
 
@@ -77,14 +94,12 @@ while long(duration) > long(position):
 			next_i += 1
 		elif i == next_i:
 			sys.stdout.write(str(subtitles[i].content) + "\n")
-			#logging.debug(str(subtitles[i].content))
 			next_i += 1
 	elif long(position) > long(end):
 		if subtitles[i] == subtitles[-1]:
 			i = 0
 			next_i = 0
-			sys.stdout.write(str(datetime.now()) + "\n")
-			#logging.debug(str(datetime.now()))
+			#sys.stdout.write(str(datetime.now()) + "\n")
 		else:
 			i += 1
 	elif long(position) == 0:
